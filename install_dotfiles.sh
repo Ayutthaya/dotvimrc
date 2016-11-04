40b89e452de6091bf75e1d9b514e95d353a0e8a3
Nathanael Foy <nfoy@srv003010.fr.cfm.fr>
2016-11-04 16:54:44 +0100

    refactor bashrc, install scripts

---
+++ install_dotfiles.sh
@@ -0,0 +1,5 @@
+cd ~
+ln -s ~/.vim/vimrc ~/.vimrc
+ln -s ~/.vim/bashrc ~/.bashrc
+ln -s ~/.vim/tmux.conf ~/.tmux.conf
+ln -s ~/.vim/git-completion.bash ~/.git-completion.bash
--- install_vim_plugins.sh
+++ install_vim_plugins.sh
@@ -1,6 +1,3 @@
-cd ~
-ln -s ~/.vim/vimrc ~/.vimrc
-cd ~/.vim
 # Register the submodules
 git submodule init
 # Checks out the version of the submodules that were committed
