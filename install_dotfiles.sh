6e5657bc5fb65bc10b1bc6e2a43ba7876ce080c7
nath <nathanael.foy@gmail.com>
2017-11-25 16:30:08 +0100

    Create a symbolic link .vim to dotvimrc

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -1,5 +1,12 @@
 cd ~
-ln -s ~/.vim/dotfiles/vimrc ~/.vimrc
-ln -s ~/.vim/dotfiles/bashrc ~/.bashrc
-ln -s ~/.vim/dotfiles/tmux.conf ~/.tmux.conf
-ln -s ~/.vim/dotfiles/git-completion.bash ~/.git-completion.bash
+ln -s ~/dotvimrc/dotfiles/vimrc ~/.vimrc
+ln -s ~/dotvimrc/dotfiles/bashrc ~/.bashrc
+ln -s ~/dotvimrc/dotfiles/tmux.conf ~/.tmux.conf
+ln -s ~/dotvimrc/dotfiles/git-completion.bash ~/.git-completion.bash
+
+if [ -e ~/.vim ]
+then
+    echo "~/.vim already exists"
+else
+    ln -s ~/dotvimrc ~/.vim
+fi
